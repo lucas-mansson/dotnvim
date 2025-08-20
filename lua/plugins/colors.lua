a0a341ff243ebacaa745d3e95e01a5fe14d088bc
Lucas Månsson <lu5132ma-s@student.lu.se>
2025-08-20 10:51:26 +0200

    Update colors.lua

--- lua/plugins/colors.lua
+++ lua/plugins/colors.lua
@@ -2,7 +2,6 @@ return {
   "navarasu/onedark.nvim",
   priority = 1000, -- make sure to load this before all the other start plugins
   config = function()
-    -- Enable theme
     require('onedark').load()
   end
 }
