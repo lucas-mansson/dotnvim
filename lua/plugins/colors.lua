83660b4504a0bd429dd10d001de32e57fe5cc5e9
lucas-mansson <lu5132ma-s@student.lu.se>
2025-10-29 11:34:20 +0100

    add vimtex and change color scheme

--- lua/plugins/colors.lua
+++ lua/plugins/colors.lua
@@ -1,7 +1,13 @@
 return {
-  "navarasu/onedark.nvim",
+  --[["navarasu/onedark.nvim",
   priority = 1000, -- make sure to load this before all the other start plugins
   config = function()
     require('onedark').load()
+  end]]
+  "rose-pine/neovim",
+  name = "rose-pine",
+  priority = 1000, -- make sure to load this before all the other start plugins
+  config = function()
+    vim.cmd("colorscheme rose-pine")
   end
 }
