2f0e9d454936303ce2316ace4fc48171f50697b6
lucas-mansson <lu5132ma-s@student.lu.se>
2025-11-04 21:19:01 +0100

    add command for live grep

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -14,10 +14,15 @@ return {
       -- Files that are just in a git repo: Ctrl+P
       vim.keymap.set('n', '<C-p>', builtin.git_files, { desc = 'Telescope find git files' })
 
-      -- Run grep command: space -> p -> s
       -- Requires ripgrep
+      -- Run grep command: space -> p -> s
       vim.keymap.set('n', '<leader>ps', function()
         builtin.grep_string({ search = vim.fn.input("Grep > ") })
+      end, { desc = 'Telescope grep string input' })
+
+      -- "Live grep", brings up telecope and searchable grep
+      vim.keymap.set('n', '<leader>lg', function()
+        builtin.live_grep()
       end, { desc = 'Telescope grep string input' })
     end,
   },
