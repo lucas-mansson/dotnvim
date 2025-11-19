a96dc27a107130173dd430bd2415db8bff9e1010
lucas-mansson <lu5132ma-s@student.lu.se>
2025-11-19 10:31:37 +0100

    fix transparent context

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -80,5 +80,10 @@ return {
       zindex = 20,
       on_attach = nil,
     },
+    config = function()
+      -- Make sure that background is transparent for context also
+      vim.api.nvim_set_hl(0, "TreesitterContext", { bg = "NONE" })
+      vim.api.nvim_set_hl(0, "TreesitterContextLineNumber", { bg = "NONE" })
+    end
   }
 }
