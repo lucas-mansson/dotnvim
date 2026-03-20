bd877f21695151d176fe358da38f29b4ef57b0fb
lucas-mansson <lu5132ma-s@student.lu.se>
2026-03-20 10:31:30 +0100

    fix no neck pain buffer

--- lua/plugins/noneckpain.lua
+++ lua/plugins/noneckpain.lua
@@ -1,15 +1,17 @@
 return {
   "shortcuts/no-neck-pain.nvim",
   version = "*",
-  width = 120,
   config = function()
-    --vim.cmd("NoNeckPain")
     require("no-neck-pain").setup({
       width = 150,
+      buffers = {
+        right = {
+          enabled = false,
+        },
+      },
       autocmds = {
         enableOnVimEnter = true,
-      }
+      },
     })
-  end
-
+  end,
 }
